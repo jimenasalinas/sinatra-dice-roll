9305bd6c097427b920a70ff27fa9186179cef3c1
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-20 18:35:34 +0000

    adding 2 dice

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "puma"
+gem "sinatra-contrib"
 
 #then use "bundle install" in the terminal
--- lucky_number.rb
+++ lucky_number.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "sinatra/reloader"
 
 get("/howdy") do
   return "Hello, world!"
@@ -10,4 +11,19 @@ end
 get('/') do
   lucky_num = rand(100)
   return "Your lucky number for today is " + lucky_num.to_s
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
