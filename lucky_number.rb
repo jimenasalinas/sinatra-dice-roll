42b77df6610e650853ac34b386217b6302293cbe
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-20 20:36:48 +0000

    added templates

--- lucky_number.rb
+++ lucky_number.rb
@@ -16,17 +16,8 @@ end
 
 # Now root url
 
-get('/') do
-
-  "<h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/2/20\">Roll two 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>"
-
+get("/") do
+  erb(:elephant)
 end
 
 get("/giraffe") do
@@ -38,10 +29,9 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 
@@ -50,20 +40,18 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 
 get("/dice/1/20") do
   first_die = rand(1..20)
 
-  outcome = "You rolled a #{first_die} !"
+  @outcome = "You rolled a #{first_die} !"
 
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/2/20") do
@@ -71,10 +59,8 @@ get("/dice/2/20") do
   second_die = rand(1..20)
   sum = first_die + second_die
 
-  outcome =  "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-
-  "<h1>2d20</h1>
-  <p>#{outcome}</p>"
+  @outcome =  "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  erb(:two_twenty)
 end
 
 get("/dice/5/4") do
@@ -85,8 +71,7 @@ get("/dice/5/4") do
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + forth_die + fifth_die
 
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{forth_die}, and a #{fifth_die} for a total of #{sum}"
+  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{forth_die}, and a #{fifth_die} for a total of #{sum}"
 
-  "<h1>4d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
 end
