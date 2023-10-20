582482c6f2800f567db637bcb0bf0b84d78ad978
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-20 19:06:04 +0000

    adding diff die cases

--- lucky_number.rb
+++ lucky_number.rb
@@ -27,3 +27,50 @@ get("/dice/2/6") do
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
+
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  "<h1>2d10</h1>
+  <p>#{outcome}</p>"
+end
+
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+
+  outcome = "You rolled a #{first_die} !"
+
+  "<h1>1d20</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/2/20") do
+  first_die = rand(1..20)
+  second_die = rand(1..20)
+  sum = first_die + second_die
+
+  outcome =  "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  "<h1>2d20</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  forth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + forth_die + fifth_die
+
+  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{forth_die}, and a #{fifth_die} for a total of #{sum}"
+
+  "<h1>4d4</h1>
+  <p>#{outcome}</p>"
+end
