58573c6fc1d58f56bc633b37afe44b145008f31a
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-21 01:32:13 +0000

    finished looping section

--- lucky_number.rb
+++ lucky_number.rb
@@ -25,53 +25,77 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  erb(:two_six)
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
 
   erb(:two_ten)
 end
 
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} !"
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
 
   erb(:one_twenty)
 end
 
-get("/dice/2/20") do
-  first_die = rand(1..20)
-  second_die = rand(1..20)
-  sum = first_die + second_die
 
-  @outcome =  "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+get("/dice/2/20") do
+  @rolls = []
+
+  2.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
   erb(:two_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  forth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + forth_die + fifth_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{forth_die}, and a #{fifth_die} for a total of #{sum}"
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
 
   erb(:five_four)
+end
+
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:hundred_six)
 end
